b4d573170503c00782324c6055f5fcc87b299121
Zarko Radenkovic <zradenkovic520rn@raf.rs>
2022-12-21 21:57:59 +0100

    Initial commit

---
+++ src/main/java/com/komponente/KorisnickiServis2/KorisnickiServis2Application.java
@@ -0,0 +1,13 @@
+package com.komponente.KorisnickiServis2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorisnickiServis2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KorisnickiServis2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/komponente/KorisnickiServis2/KorisnickiServis2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.komponente.KorisnickiServis2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KorisnickiServis2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
